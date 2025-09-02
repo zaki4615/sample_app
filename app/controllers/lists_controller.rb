51e00c14c1b3124acfee89c2be9a581a174e3a02
WataruKanzaki <224184043+zaki4615@users.noreply.github.com>
2025-09-02 02:08:56 +0000

    Initial Commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,40 @@
+class ListsController < ApplicationController
+  def new
+     @list = List.new
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to list_path(list.id)
+  end
+  
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end 
+
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end  
+end
